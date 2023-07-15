74d126253f9e8bcfcdd72fbf491f037fdeffc97c
ccguself <ccguself@163.com>
2023-07-15 12:41:02 +0800

    回测与逐笔数据使用

---
+++ new_and_prepare/backtest/constant.py
@@ -0,0 +1,36 @@
+"""
+General constant enums used in the trading platform.
+"""
+
+from enum import Enum
+
+
+class Direction(Enum):
+    """
+    Direction of order/trade/position.
+    """
+    LONG = "多"
+    SHORT = "空"
+    NET = "净"
+
+class Status(Enum):
+    """
+    Order status.
+    """
+    SUBMITTING = "提交中"
+    NOTTRADED = "未成交"
+    PARTTRADED = "部分成交"
+    ALLTRADED = "全部成交"
+    CANCELLED = "已撤销"
+    REJECTED = "拒单"
+
+class OrderType(Enum):
+    """
+    Order type.
+    """
+    LIMIT = "限价"
+    MARKET = "市价"
+    STOP = "STOP"
+    FAK = "FAK"
+    FOK = "FOK"
+    RFQ = "询价"
---
+++ new_and_prepare/backtest/object.py
@@ -0,0 +1,58 @@
+"""
+Basic data structure used for general trading function in the trading platform.
+"""
+
+from dataclasses import dataclass, field
+from datetime import datetime
+from logging import INFO
+
+from .constant import Direction, Status, OrderType
+
+@dataclass
+class OrderData:
+    """
+    Order data contains information for tracking lastest status
+    of a specific order.
+    """
+
+    symbol: str
+    orderid: str
+
+    type: OrderType = OrderType.LIMIT
+    direction: Direction = None
+    price: float = 0
+    volume: float = 0
+    traded: float = 0
+    status: Status = Status.SUBMITTING
+    datetime: datetime = None
+    reference: str = ""
+
+    def __post_init__(self) -> None:
+        """"""
+        self.vt_symbol: str = f"{self.symbol}.{self.exchange.value}"
+        self.vt_orderid: str = f"{self.gateway_name}.{self.orderid}"
+
+@dataclass
+class OrderBook:
+    """
+    订单簿指，某一特定价格下的订单
+    """
+    direciton: Direction = None
+    price: float = 0
+    
+
+
+
+
+class Snapshot:
+    def __init__(self, symbol, datetime) -> None:
+        self.symbol = symbol
+        self.datetime = datetime
+
+        pass
+
+
+
+
+
+
