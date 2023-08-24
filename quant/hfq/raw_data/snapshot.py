4c6650434da28a93f832008f7f2e95813d331231
ccguself <ccguself@163.com>
2023-08-24 21:46:25 +0800

    structure

---
+++ quant/hfq/raw_data/snapshot.py
@@ -0,0 +1,18 @@
+import ray
+import numpy as np
+import pandas as pd
+
+class Snapshot:
+    def __init__(self) -> None:
+        pass
+
+    def load_data(self):
+        return
+    
+    def process_data(self):
+        return 
+    
+    def get_features(self):
+        return
+    
+    
