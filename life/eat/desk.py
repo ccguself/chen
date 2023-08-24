4243f1b5c06105b5366e11369f46f16db5e07e95
ccguself <ccguself@163.com>
2023-08-24 23:29:40 +0800

    structure

---
+++ life/eat/desk.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+
+class Desk:
+    def __init__(self) -> None:
+        pass
+
+    def get_ingredient(self):
+        """
+            Obtain the data.
+        """
+        return
+    
+    def check_ingredient(self):
+        """
+            Analysis the data, evaluate the feature.
+        """
+        return
+    
+    def wash_ingredient(self):
+        """
+            Get the semi, e.g. features and labels.
+        """
+        return 
+
+    def cook(self):
+        """
+            Joint features and labels.
+        """
+        return 
+
+    def taste_food(self):
+        """
+        """
+        return 
+    
+    
+    
---
+++ life/eat/pipeline/ingredient/meat.py
@@ -0,0 +1,18 @@
+import ray
+import numpy as np
+import pandas as pd
+
+class Tick:
+    def __init__(self) -> None:
+        pass
+
+    def load_data(self):
+        return
+    
+    def process_data(self):
+        return 
+    
+    def get_features(self):
+        return
+    
+    
